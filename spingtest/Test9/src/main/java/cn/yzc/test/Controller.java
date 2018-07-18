2ca857076cc36bda07a668289fab1fc5a55c4064
拯救者 <yzchwork@outlook.com>
2018-07-18 18:00:11 +0800

    first

---
+++ spingtest/Test9/src/main/java/cn/yzc/test/Controller.java
@@ -0,0 +1,28 @@
+package cn.yzc.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/myController")
+public class Controller extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // System.err.println("---");
+        //解决乱码
+        req.setCharacterEncoding("UTF-8");
+        String name=req.getParameter("name");
+        req.setAttribute("name",name);
+        System.out.println(name);
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+
+}
---
+++ spingtest/Test9/src/main/webapps/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 拯救者
+  Date: 2018/7/18
+  Time: 14:41
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="myController" method="post">
+    <input name="name">
+    return:${name}
+    <input value="提交" type="submit">
+</form>
+</body>
+</html>
